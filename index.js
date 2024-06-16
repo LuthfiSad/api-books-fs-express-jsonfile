02192d1e4cedafe09674224aba4205a8706e1698
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-16 14:02:46 +0700

    fix: vercel auth

--- index.js
+++ index.js
@@ -14,53 +14,65 @@ server.use(middlewares);
 server.use(apiPrefix);
 server.use(jsonServer.bodyParser);
 
-
 const getIndonesiaTime = () => {
   const timeZone = 'Asia/Jakarta';
   const date = new Date();
-  return formatInTimeZone(date, timeZone, 'yyyy-MM-dd\'T\'HH:mm:ss\'Z\'');
+  return formatInTimeZone(date, timeZone, "yyyy-MM-dd'T'HH:mm:ss'Z'");
 };
 
 server.post('/books/donate', (req, res) => {
-  const db = router.db;
-  
-  req.body.createdAt = getIndonesiaTime();
-  req.body.borrowCount = 0;
-  req.body.id = Date.now();
-  
-  // Write to the in-memory database
-  db.get('books').push(req.body).write();
-  
-  return res.status(201).json(req.body);
+  try {
+    const db = router.db;
+    req.body.createdAt = getIndonesiaTime();
+    req.body.borrowCount = 0;
+    req.body.id = Date.now();
+    
+    // Write to the in-memory database
+    db.get('books').push(req.body).write();
+    
+    return res.status(201).json(req.body);
+  } catch (error) {
+    console.error('Error during book donation:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Handle PUT request to update a book
 server.put('/books/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const updatedBook = req.body;
-  const db = router.db; // LowDB instance
-  const book = db.get('books').find({ id }).value();
-  // return res.status(405).json(book);
+  try {
+    const id = parseInt(req.params.id, 10);
+    const updatedBook = req.body;
+    const db = router.db; // LowDB instance
+    const book = db.get('books').find({ id }).value();
 
-  if (book) {
-    db.get('books').find({ id }).assign(updatedBook).write();
-    res.status(200).json({ message: `Book with ID ${id} updated successfully.` });
-  } else {
-    res.status(404).json({ error: 'Book not found.' });
+    if (book) {
+      db.get('books').find({ id }).assign(updatedBook).write();
+      return res.status(200).json({ message: `Book with ID ${id} updated successfully.` });
+    } else {
+      return res.status(404).json({ error: 'Book not found.' });
+    }
+  } catch (error) {
+    console.error('Error during book update:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
 // Handle DELETE request to delete a book
 server.delete('/books/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const db = router.db; // LowDB instance
-  const book = db.get('books').find({ id }).value();
+  try {
+    const id = parseInt(req.params.id, 10);
+    const db = router.db; // LowDB instance
+    const book = db.get('books').find({ id }).value();
 
-  if (book) {
-    db.get('books').remove({ id }).write();
-    res.status(200).json({ message: `Book with ID ${id} deleted successfully.` });
-  } else {
-    res.status(404).json({ error: 'Book not found.' });
+    if (book) {
+      db.get('books').remove({ id }).write();
+      return res.status(200).json({ message: `Book with ID ${id} deleted successfully.` });
+    } else {
+      return res.status(404).json({ error: 'Book not found.' });
+    }
+  } catch (error) {
+    console.error('Error during book deletion:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
